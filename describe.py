1f3a103dccd7fec5057b88b9d1ea2a851cde6fae
pac-nam <thomasbleuse02@gmail.com>
2020-04-24 12:03:55 +0200

    get_dataset done

--- describe.py
+++ describe.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-def main():
+def get_dataset():
     f = open(sys.argv[1],"r")
     csv_reader = csv.reader(f, delimiter=',')
     count_line = 0
@@ -9,15 +9,18 @@ def main():
     for line in csv_reader:
         if (count_line == 0):
             len_data  = len(line)
+            for j in range (0, len_data):
+                dataset.append([])
         if (len(line) != len_data):
             print("File not well formed")
             return
         count_row = 0
         for row in line :
+            dataset[count_row] += {row}
             count_row += 1
         count_line += 1
-    print(dataset)
     f.close()
+    return (dataset)
 
 if __name__ == "__main__":
-    main()
+    print(get_dataset())
