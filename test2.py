c6b207803de936d864730e265bf4144981994d7d
ayguillo <ayguillo@student.42.fr>
2020-05-29 17:12:49 +0200

    test

---
+++ test2.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+import seaborn as sns
+import random
+
+i = 0
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def compute_cost(X, y, theta):
+	global i
+	m = len(y)
+	h = sigmoid(X @ theta)
+	if i == 2:
+		print(theta)
+		# print(h)
+	epsilon = 0
+	cost = (1/m)*(((-y).T @ np.log(h + epsilon))-((1-y).T @ np.log(1-h + epsilon)))
+	i += 1
+	return cost
+
+def gradient_descent(X, y, params, learning_rate, iterations):
+	m = len(y)
+	cost_history = np.zeros((iterations,1))
+
+	for i in range(iterations):
+		params = params - (learning_rate/m) * (X.T @ (sigmoid(X @ params) - y)) 
+		cost_history[i] = compute_cost(X, y, params)
+
+	return (cost_history, params)
+
+def predict(X, params):
+	return np.round(sigmoid(X @ params))
+
+X, y = make_classification(n_samples=500, n_features=2, n_redundant=0, n_informative=5,
+							 n_clusters_per_class=5, random_state=random.choice(range(15)))
+
+y = y[:,np.newaxis]
+
+sns.set_style('white')
+sns.scatterplot(X[:,0],X[:,1],hue=y.reshape(-1))
+
+m = len(y)
+
+X = np.hstack((np.ones((m,1)),X))
+n = np.size(X,1)
+params = np.zeros((n,1))
+
+iterations = 1500
+learning_rate = 0.03
+
+initial_cost = compute_cost(X, y, params)
+
+print("Initial Cost is: {} \n".format(initial_cost))
+
+(cost_history, params_optimal) = gradient_descent(X, y, params, learning_rate, iterations)
+
+print("Optimal Parameters are: \n", params_optimal, "\n")
+
+plt.figure()
+sns.set_style('white')
+plt.plot(range(len(cost_history)), cost_history, 'r')
+plt.title("Convergence Graph of Cost Function")
+plt.xlabel("Number of Iterations")
+plt.ylabel("Cost")
+plt.show()
+
+y_pred = predict(X, params_optimal)
+score = float(sum(y_pred == y))/ float(len(y))
+
+print(score)
+slope = -(params_optimal[1] / params_optimal[2])
+intercept = -(params_optimal[0] / params_optimal[2])
+
+sns.set_style('white')
+sns.scatterplot(X[:,1],X[:,2],hue=y.reshape(-1));
+
+ax = plt.gca()
+ax.autoscale(False)
+x_vals = np.array(ax.get_xlim())
+y_vals = intercept + (slope * x_vals)
+plt.plot(x_vals, y_vals, c="k")
+plt.show()
