8139573d7e1dbec5f606bff478823f1542bb31dc
pac-nam <thomasbleuse02@gmail.com>
2020-06-12 15:46:04 +0200

    1.8% de diff

---
+++ logreg_predict.py
@@ -0,0 +1,53 @@
+import numpy as np
+import argparse
+import pandas as pd
+import os
+import sys
+from normalize import normalize_numpy
+
+def predict_this_stud(notes, thetas):
+	house = 0
+	best = thetas[0][0] + notes[0] * thetas[0, 1] + notes[1] * thetas[0, 2] + notes[2] * thetas[0, 3]
+	# res = "G: " + str(best) + " | "
+	# print(notes[0])
+	# sys.exit()
+	for i in range (1, 4):
+		tmp = thetas[i, 0] + notes[0] * thetas[i, 1] + notes[1] * thetas[i, 2] + notes[2] * thetas[i, 3]
+		# res += " | " + str(i) + ": " + str(tmp)
+		if tmp > best:
+			best = tmp
+			house = i
+	# return (res)
+	if house == 0:
+		return "Gryffindor"
+	if house == 1:
+		return "Slytherin"
+	if house == 2:
+		return "Ravenclaw"
+	if house == 3:
+		return "Hufflepuff"
+
+def compute(data):
+	notes = normalize_numpy(data[["Astronomy", "Ancient Runes", "Herbology"]].copy().to_numpy())
+	notes[np.isnan(notes)] = 0.5
+	thetas = pd.read_csv(".save_model.csv", delimiter=',').to_numpy()
+	res = "Index,Hogwarts House"
+	for i in range(len(notes)):
+		res += "\n" + str(i) + "," + predict_this_stud(notes[i], thetas)
+	f = open("houses.csv", "w")
+	f.write(res)
+	f.close()
+	
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("file", help="define your file", type = str)
+	args = parser.parse_args()
+	if not os.path.exists(args.file) or not os.path.isfile(args.file):
+		print("File error:", args.file)
+		sys.exit()
+	data = pd.read_csv(args.file)
+	if "Ancient Runes" not in data.columns or "Astronomy" not in data.columns or "Herbology" not in data.columns:
+		print('Need columns: "Ancient Runes", "Astronomy", "Herbology"')
+	else:
+		compute(data)
--- logreg_train.py
+++ logreg_train.py
@@ -13,16 +13,16 @@ from reg_fit import regression
 def save_theta(thetas):
 	with open(".save_model.csv", 'w', newline='') as csvfile:
   		spamwriter = csv.writer(csvfile, delimiter=',')
-  		spamwriter.writerow(["Theta0", "Theta1", "Theta2"])
+  		spamwriter.writerow(["Theta0", "Theta1", "Theta2", "Theta3"])
   		spamwriter.writerow(thetas[0])
   		spamwriter.writerow(thetas[1])
   		spamwriter.writerow(thetas[2])
   		spamwriter.writerow(thetas[3])
 
 def get_notes_houses(data):
-	new_df = data[["Hogwarts House", "Astronomy", "Ancient Runes"]].copy().dropna()
-	notes = normalize_numpy(new_df[["Astronomy", "Ancient Runes"]].copy().to_numpy())
-	notes = np.hstack((np.ones((len(notes),1)), notes))
+	new_df = data[["Hogwarts House", "Astronomy", "Ancient Runes", "Herbology"]].copy().dropna()
+	notes = normalize_numpy(new_df[["Astronomy", "Ancient Runes", "Herbology"]].copy().to_numpy())
+	notes = np.hstack((np.ones((len(notes), 1)), notes))
 	houses = new_df["Hogwarts House"].replace(
 		["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
 		[1, 2, 3, 4])
@@ -33,7 +33,8 @@ def single_house_regression(notes, houses, house_number):
 	binary_houses = np.where(houses!=house_number, 0, houses)
 	binary_houses = np.where(binary_houses==house_number, 1, binary_houses)
 	# print(binary_houses)
-	x = regression(notes, binary_houses, np.zeros((3, 1)), 0.1, 100)
+	# return ("hello")
+	x = regression(notes, binary_houses, np.zeros((4, 1)), 5, 7000)
 	x.gradient_descent()
 	# print(x.theta)
 	return(x.theta.T.tolist())
@@ -46,6 +47,7 @@ def compute(data):
 	# save_theta(houses_theta)
 	# return
 
+	print(notes)
 	for i in range (1, 5):
 		houses_theta += single_house_regression(notes, houses, i)
 	save_theta(houses_theta)
