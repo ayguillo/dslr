ede60f68e2fe65316486408366e06d2bf2b925da
pac-nam <thomasbleuse02@gmail.com>
2020-06-12 12:08:43 +0200

    nan removed from training

--- logreg_train.py
+++ logreg_train.py
@@ -8,13 +8,25 @@ import sys
 from normalize import normalize_numpy
 from reg_fit import regression
 
-def get_notes(data):
-	new_df = data[["Astronomy", "Ancient Runes"]].copy()
-	new_df = normalize_numpy(new_df.to_numpy())
-	# print(len(new_df))
-	new_df = np.hstack((np.ones((len(new_df),1)), new_df))
-	# print(new_df)
-	return new_df
+
+def save_theta(theta0, theta1, theta2):
+	with open(".save_model.csv", 'w', newline='') as csvfile:
+  	  spamwriter = csv.writer(csvfile, delimiter=',')
+  	  spamwriter.writerow(["Theta0", "Theta1", "Theta2"])
+  	  spamwriter.writerow([theta0, theta1, theta2])
+
+def get_notes_houses(data):
+	new_df = data[["Hogwarts House", "Astronomy", "Ancient Runes"]].copy().dropna()
+	notes = normalize_numpy(new_df[["Astronomy", "Ancient Runes"]].copy().to_numpy())
+	# print(notes)
+	notes = np.hstack((np.ones((len(notes),1)), notes))
+	houses = data["Hogwarts House"].replace(
+		["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+		[1, 2, 3, 4])
+	houses = houses.to_numpy()[:,np.newaxis]
+	print(notes)
+	# print(houses)
+	return (notes, houses)
 
 def get_houses(data):
 	new_df = data["Hogwarts House"].replace(
@@ -22,13 +34,29 @@ def get_houses(data):
 		[1, 2, 3, 4])
 	return new_df.to_numpy()[:,np.newaxis]
 
+def single_house_regression(notes, houses, house_number):
+	binary_houses = np.where(houses!=house_number, 0, houses)
+	binary_houses = np.where(binary_houses==house_number, 1, binary_houses)
+	# print(binary_houses)
+	x = regression(notes, binary_houses, np.zeros((3, 1)), 0.1, 100)
+	# print(x.gradient_descent())
+	# print(x.theta.T.tolist())
+	return(x.theta.T)
+
+# def clean_nan(notes, houses):
+
 def compute(data):
-	# a = get_notes(data)
-	# print(get_houses(data))
+	notes, houses = get_notes_houses(data)
+	# houses = get_houses(data)
+	# notes, houses = clean_nan(notes, houses)
+	# houses_theta = []
 	# print(np.zeros((3, 1)))
-	x = regression(get_notes(data), get_houses(data), np.zeros((3, 1)), 0.1, 100)
-	print(x.gradient_descent())
-	print(x.theta)
+	# for i in range (1, 5):
+	single_house_regression(notes, houses, 2)
+	# print(houses_theta)
+	# x = regression(get_notes(data), get_houses(data), np.zeros((3, 1)), 0.1, 100)
+	# print(x.gradient_descent())
+	# print("\nthetas:\n", x.theta)
 	
 
 if __name__ == "__main__":
